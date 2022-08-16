e73673871cb67a1560758e0c589e1749ddea64e4
Helinka <fedel1802@yandex.ru>
2022-08-16 22:38:17 +0300

    task19 and gitignore add

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string a = Convert.ToString(n);
+
+if ( a.Length == 5 )
+{
+    if (a[0] == a[4] && a[1] == a[3])
+    {
+        Console.WriteLine("Это палиндром!");
+    }
+    else
+    {
+        Console.WriteLine("Это не палиндром!");
+    }
+}
+else
+{
+    Console.WriteLine("Это не пятизначное число");
+}
